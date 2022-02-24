be569db88265a7c4cde3f6161dc1e2f744a857ac
BHowesCS <BHowesCS@gmail.com>
2022-02-24 14:05:57 -0500

    Updated VRS function with ELIF & ELSE statements - stable

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -13,7 +13,24 @@ weatherAlert = weather()
 
 def vehicleResponseSystem():
     if weatherAlert == "Ice":
-        print (Fore.RED + "\nVRS has changed your alarm 30 minutes early due to " + Fore.LIGHTBLUE_EX + "ICY ROADS!")
+        print (Fore.RED + "\nVRS has changed your alarm 30 minutes early due to " + Fore.BLUE + "ICY ROADS!")
         print (Fore.RED + "VRS will only allow your car to go 30 MPH" + Fore.RESET)
+    elif weatherAlert == "Snow":
+        print(Fore.RED + "\nVRS has changed your alarm 15 minutes early due to " + Fore.BLUE + "SNOW!")
+        print(Fore.RED + "VRS will only allow your car to go 50 MPH" + Fore.RESET)
+    elif weatherAlert == "Heavy Rain":
+        print(Fore.RED + "\nVRS has changed your alarm 10 minutes early due to " + Fore.BLUE + " HEAVY RAIN!")
+        print(Fore.RED + "VRS will only allow your car to go 50 MPH" + Fore.RESET)
+    elif weatherAlert == "Rain":
+        print(Fore.RED + "\nVRS has changed your alarm 5 minutes early due to " + Fore.BLUE + "RAIN!")
+        print(Fore.RED + "VRS will only allow your car to go 60 MPH" + Fore.RESET)
+    elif weatherAlert == "High Wind":
+        print(Fore.RED + "\nVRS has changed your alarm 5 minutes early due to " + Fore.BLUE + "HIGH WIND!")
+        print(Fore.RED + "VRS will only allow your car to go 70 MPH" + Fore.RESET)
+    elif weatherAlert == "Fog":
+        print(Fore.RED + "\nVRS has changed your alarm 5 minutes early due to " + Fore.BLUE + "FOG!")
+        print(Fore.RED + "VRS will only allow your car to go 60 MPH" + Fore.RESET)
+    else:
+        print(Fore.YELLOW + "\nWeather is Sunny!" + Fore.RESET)
 
 vehicleResponseSystem()
