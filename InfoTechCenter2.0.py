c70a32aa4fc9dd112c487a941aa6576adf9f7e6e
BHowesCS <BHowesCS@gmail.com>
2022-03-01 13:32:21 -0500

    Completed Gas Level Alert Function And Is Stable

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -1,18 +1,29 @@
 import random
 from colorama import Fore
 
-#Gas Level Function
+# Gas Level Function
 def gasLevelGauge():
-    gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full"]
+    gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full"]
     currentGasLevel = random.choice(gasLevelList)
-    #print(currentGasLevel)
     return currentGasLevel
 
-#Create IF-ELIF-ELSE statements using Comparative Operators to display gas level messages
+# Variable calls the value of gasLevelGauge Function
+gasLevelIndicator = gasLevelGauge()
+
+# Create IF-ELIF-ELSE statements using Comparative Operators == Equal To in order to display gas level messages
 def gasLevelAlert():
-    if gasLevelGauge() == "Empty":
-        print(Fore.RED+"     ***WARNING***\nYou have run out of gas"+Fore.RESET)
+    if gasLevelIndicator == "Empty":
+        print(Fore.RED+"***WARNING***\n**GAS EMPTY**\nCalling Emergency Contact"+Fore.RESET)
+    elif gasLevelIndicator == "Low":
+        print(Fore.LIGHTYELLOW_EX+"***WARNING***\n**LOW ON GAS**\nThe closest gas station is "+Fore.RESET)
+    elif gasLevelIndicator == "Quarter Tank":
+        print(Fore.YELLOW+"QUARTER TANK Of Gas\nPlan On Visiting The Gas Station"+Fore.RESET)
+    elif gasLevelIndicator == "Half Tank":
+        print(Fore.LIGHTGREEN_EX+"HALF TANK Of Gas\n125 Miles Till Empty"+Fore.RESET)
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print(Fore.GREEN+"THREE QUARTER TANK Of Gas\n205 Miles Till Empty"+Fore.RESET)
+    else:
+        print(Fore.GREEN+"FULL TANK Of Gas\n310 Miles Till Empty"+Fore.RESET)
 
-
-#gasLevelGauge()
+# Call Functions Here
 gasLevelAlert()
