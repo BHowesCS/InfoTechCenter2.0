df2a53a81cc8b40f9b99d55cee94fff4ca0f4fef
BHowesCS <BHowesCS@gmail.com>
2022-03-01 13:25:32 -0500

    Import colorama to add color

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -1,8 +1,13 @@
-#Code Name - Hornet
+# Code Name - Hornet
 
-#Print to one line with time delay between prints
+# Print to one line with time delay between prints
 from time import sleep
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(strip=False, autoreset=True)
 
-print("Welcome to Hornet's InfoTechCenter\n");sleep(1)
+print(Fore.MAGENTA+"Welcome to Hornet's InfoTechCenter\n")
+
+sleep(1)
 
 print("Hornet's Operating System Booting Up")
