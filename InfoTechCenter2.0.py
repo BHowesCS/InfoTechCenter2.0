44e0cfa36c324811b5b180b649102631e67110c7
BHowesCS <BHowesCS@gmail.com>
2022-03-01 13:24:39 -0500

    Import time library

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -1,5 +1,8 @@
 #Code Name - Hornet
 
-print("Welcome to Hornet's InfoTechCenter\n")
+#Print to one line with time delay between prints
+from time import sleep
+
+print("Welcome to Hornet's InfoTechCenter\n");sleep(1)
 
 print("Hornet's Operating System Booting Up")
